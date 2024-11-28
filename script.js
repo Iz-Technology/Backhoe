64efde5fb45de59fadfc0e80539a079efdcb9755
Iz-Technology <iz.technology.94@gmail.com>
2024-11-28 19:55:16 +0800

    memperbaharui gallery, november dan script

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Data manual untuk jam
   var dailyHours = [
     0, 0, 8, 8, 8, 8, 8, 2, 8, 8, 9, 9, 10, 2, 10, 3, 9, 9, 4, 8, 9, 8, 8, 2, 1,
-    4,
+    4, 0, 3,
   ];
 
   // Hitung total jam
